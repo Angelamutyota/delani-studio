5cd72d422e19bc8bc31a2a6ecc47b94b22fffd1d
ANGELA <angela.mbithe@student.moringaschool.com>
2021-04-19 17:56:15 +0300

    add alert function on contact form

--- JS/script.js
+++ JS/script.js
@@ -55,3 +55,19 @@ $(document).ready(function(){
       $("#giraffe1").hide();
     });
 });
+$(document).ready(function(){
+  $("form#contactUs").submit(function(event){
+    event.preventDefault();
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var message = $("textarea#message").val();
+    if ($("input#name").val() && $("input#email").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+    
+  });
+
+});
