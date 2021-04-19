0390d7279e3bac454fe6ad7e35d4e14917e53433
ANGELA <angela.mbithe@student.moringaschool.com>
2021-04-19 09:58:45 +0300

    add toggle function

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
 
-$("#design").click(function() {
-    $("#design1").toggle();
+$(".click1").click(function() {
+    $(".hide1").toggle();
     $(".show1").toggle();
+});
+$(".click2").click(function() {
+    $(".hide2").toggle();
+    $(".show2").toggle();
+})
+$(".click3").click(function(){
+    $(".hide3").toggle();
+    $(".show3").toggle();
 })
 })
