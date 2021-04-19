8bb966bfa157bd96b43de0896d58b6ff7e498361
ANGELA <angela.mbithe@student.moringaschool.com>
2021-04-19 17:53:39 +0300

    add hover effect

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,57 @@
 $(document).ready(function () {
 
-$(".click1").click(function() {
+ $(".click1").click(function() {
     $(".hide1").toggle();
     $(".show1").toggle();
-});
-$(".click2").click(function() {
+  });
+ $(".click2").click(function() {
     $(".hide2").toggle();
     $(".show2").toggle();
-})
-$(".click3").click(function(){
+  });
+ $(".click3").click(function(){
     $(".hide3").toggle();
     $(".show3").toggle();
+  });
 })
-})
+$(document).ready(function(){
+    $("#mzone").mouseover(function(){
+      $("#mzone1").show();
+    }).mouseout(function(){
+      $("#mzone1").hide();
+    });
+    $("#yellow").mouseover(function(){
+      $("#yellow1").show();
+    }).mouseout(function(){
+      $("#yellow1").hide();
+    });
+    $("#ontario").mouseover(function(){
+      $("#ontario1").show();
+    }).mouseout(function(){
+      $("#ontario1").hide();
+    });
+    $("#grey").mouseover(function(){
+      $("#grey1").show();
+    }).mouseout(function(){
+      $("#grey1").hide();
+    });
+    $("#socials").mouseover(function(){
+      $("#socials1").show();
+    }).mouseout(function(){
+      $("#socials1").hide();
+    });
+    $("#forms").mouseover(function(){
+      $("#forms1").show();
+    }).mouseout(function(){
+      $("#forms1").hide();
+    });
+    $("#burned").mouseover(function(){
+      $("#burned1").show();
+    }).mouseout(function(){
+      $("#burned1").hide();
+    });
+    $("#giraffe").mouseover(function(){
+      $("#giraffe1").show();
+    }).mouseout(function(){
+      $("#giraffe1").hide();
+    });
+});
